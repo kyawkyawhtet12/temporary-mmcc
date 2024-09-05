5ea80cfec63c50c16ea16519054e85a52fbd17f6
ArkarThanNaing <arkarthannaing19@gmail.com>
2024-09-05 10:11:41 +0630

    maung win

--- app/Console/Commands/MaungWinCommand.php
+++ app/Console/Commands/MaungWinCommand.php
@@ -5,16 +5,17 @@ namespace App\Console\Commands;
 use App\Models\BettingRecord;
 use Illuminate\Console\Command;
 use App\Models\FootballMaungGroup;
+use App\Services\Ballone\MaungWinService;
 use App\Services\Ballone\MaungServiceCheck;
 
-class CheckRecords extends Command
+class MaungWinCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'check:records';
+    protected $signature = 'maung:win';
 
     /**
      * The console command description.
@@ -40,21 +41,8 @@ class CheckRecords extends Command
      */
     public function handle()
     {
-        // BettingRecord::whereNull("result")
-        //             ->orWhereNull('win_amount')
-        //             ->update([
-        //                 'result' => 'No Prize',
-        //                 'win_amount' => 0
-        //             ]);
 
-        $groups = FootballMaungGroup::where('round', '335')
-            ->where('status', 1)
-            ->with(['teams'])
-            ->chunkById(100, function ($query) {
-                foreach ($query as $q) {
-                    (new MaungServiceCheck())->execute($q->teams);
-                }
-            });
+        (new MaungWinService())->calculate();
 
         echo "done";
     }
